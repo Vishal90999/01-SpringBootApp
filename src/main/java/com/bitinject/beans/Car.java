dfa0f7ca64d971e3807158307736c5e54be972c7
vishal <vishalyendole@gmail.com>
2023-10-06 09:46:14 +0530

    project of pwd encoder and decoder

---
+++ src/main/java/com/bitinject/beans/Car.java
@@ -0,0 +1,8 @@
+package com.bitinject.beans;
+
+public class Car {
+	
+public Car() {
+	System.out.println("Car class constructor");
+}
+}
---
+++ src/main/java/com/bitinject/main/CarTest.java
@@ -0,0 +1,12 @@
+package com.bitinject.main;
+
+import com.bitinject.beans.Car;
+
+public class CarTest {
+
+	public static void main(String[] args) {
+		
+		Car obj=new Car();
+	}
+
+}
